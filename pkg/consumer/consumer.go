1d598507825a560e45934be346db845c98ab21c1
rknizzle <ryan@autotiv.com>
2020-08-10 21:57:17 -0400

    add a consumer package and struct
            - for now it just has some basic starting values for testing but
              will eventually use a config file

---
+++ pkg/consumer/consumer.go
@@ -0,0 +1,51 @@
+package consumer
+
+import (
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+)
+
+type Consumer struct {
+	Taskserver  *machinery.Server
+	AppURL      string
+	Concurrency int
+}
+
+func NewConsumer() (*Consumer, error) {
+	c := &Consumer{
+		AppURL:      "http://localhost:8081",
+		Concurrency: 1,
+	}
+
+	taskserver, err := machinery.NewServer(&config.Config{
+		Broker:        "redis://localhost:6379",
+		ResultBackend: "redis://localhost:6379",
+	})
+	if err != nil {
+		return nil, err
+	}
+	c.Taskserver = taskserver
+
+	c.Taskserver.RegisterTasks(map[string]interface{}{
+		"handyman": c.handleTask,
+	})
+	return c, nil
+}
+
+func (c *Consumer) Start() error {
+	worker := c.Taskserver.NewWorker("worker", c.Concurrency)
+	err := worker.Launch()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Consumer) handleTask() error {
+	// send task message in HTTP req to app server
+	// get the response
+	// do something with response
+
+	return nil
+}
