535e0de34c0704ae07c30cf202a0974255b35efe
rknizzle <ryan@autotiv.com>
2020-08-12 22:22:45 -0400

    add test that verifys sendRequestToApp calls client.Do

--- pkg/consumer/consumer_test.go
+++ pkg/consumer/consumer_test.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"net/http"
+	"testing"
 )
 
 // Used to mock out HTTP requests to avoid external calls in unit tests
@@ -26,4 +27,28 @@ func createConsumerWithMockedClient() (*Consumer, error) {
 	// used a mocked HTTP client
 	c.Client = &MockClient{}
 	return c, nil
+}
+
+// test that sendRequestToApp calls client.Do
+func TestSendRequestToAppCallsClientDoUnit(t *testing.T) {
+	c, err := createConsumerWithMockedClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// mock the Do function to verify that its called
+	called := false
+	GetDoFunc = func(req *http.Request) (*http.Response, error) {
+		called = true
+		return nil, nil
+	}
+
+	_, err = c.sendRequestToApp("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if called != true {
+		t.Fatal("Client.Do was not correctly called")
+	}
 }
