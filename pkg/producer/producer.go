c0344d77fdb6c9594edd5c5f1bcb7248a752743b
rknizzle <ryan@autotiv.com>
2020-08-16 13:20:55 -0400

    add produce fxn to add a message to the message broker

--- pkg/producer/producer.go
+++ pkg/producer/producer.go
@@ -2,7 +2,9 @@ package producer
 
 import (
 	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/backends/result"
 	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
 )
 
 type Producer struct {
@@ -19,4 +21,23 @@ func NewProducer() (*Producer, error) {
 		return nil, err
 	}
 	return &Producer{Taskserver: taskserver}, nil
+}
+
+func (p *Producer) Produce(contents string) (*result.AsyncResult, error) {
+	task := tasks.Signature{
+		Name: "handyman",
+		Args: []tasks.Arg{
+			{
+				Type:  "string",
+				Value: contents,
+			},
+		},
+	}
+
+	res, err := p.Taskserver.SendTask(&task)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
